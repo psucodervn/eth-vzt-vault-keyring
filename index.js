6f9e230e7132772bf4b0ebe2401f071e1ee54a58
Hưng Lê <hunglm@vzota.com.vn>
2019-03-06 20:16:19 +0700

    Fix bug 'browser is not defined' in Chrome

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const type = "Vzt Vault";
 const EventEmitter = require("events").EventEmitter;
 const API_URL = "http://pro.vwallet.vzota.com/metamask/public/index.html";
+const browser = require("webextension-polyfill");
 
 function uuidv4() {
   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
